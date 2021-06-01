95a0c1c16ec156188d62b312b5e875eb2187767d
Ngọc Thuấn Nguyễn <thuannguyen121000@gmail.com>
2021-06-02 01:00:06 +0700

    Add some interactions with Statistic Page

--- app/src/main/java/com/example/smarthome/Fragment/StatisticsFragment.java
+++ app/src/main/java/com/example/smarthome/Fragment/StatisticsFragment.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,29 +15,78 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.example.smarthome.Activity.MainActivity;
 import com.example.smarthome.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class StatisticsFragment extends Fragment {
-    TextView tvFromDate;
-    TextView tvToDate;
-    TextView tvDateError;
-    TextView btnGo;
+    private View layoutStatisticGen, layoutStatisticSpec;
+
+    private TextView btnToday, btnYesterday, btnThisWeek, btnThisMonth;
+
+    // For Range Option
+    private TextView tvFromDate, tvToDate, tvDateError;
+    private TextView btnGo;
+
+    // On Statistic Spec Page
+    private ImageView btnBackToStatisticGen;
+    private TextView tv_statisticOption;
+    private TextView tv_AvgTempVal, tv_AvgFanOpTimeVal, tv_AvgLightIntenseVal, tv_AvgLightOpTimeVal;
     DatePickerDialog.OnDateSetListener FromDateListener;
     DatePickerDialog.OnDateSetListener ToDateListener;
+
+    // Firebase
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    private CollectionReference lightRecord;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        lightRecord = db.collection("light_records");
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_statistics, container, false);
+
+        layoutStatisticGen = view.findViewById(R.id.layoutStatisticGen);
+        layoutStatisticSpec = view.findViewById(R.id.layoutStatisticSpec);
+        layoutStatisticGen.setVisibility(View.VISIBLE);
+        layoutStatisticSpec.setVisibility(View.GONE);
+
         Context context = view.getContext();
+
+        btnToday = view.findViewById(R.id.tv_todayOption);
+        btnYesterday = view.findViewById(R.id.tv_yesterdayOption);
+        btnThisWeek = view.findViewById(R.id.tv_thisWeekOption);
+        btnThisMonth = view.findViewById(R.id.tv_thisMonthOption);
+
+        // Range Option
         tvFromDate = view.findViewById(R.id.tv_fromDate);
         tvToDate = view.findViewById(R.id.tv_toDate);
         tvDateError = view.findViewById(R.id.tvDateError);
         btnGo = view.findViewById(R.id.btnGo);
+
+        // On StatisticSpec Page
+        btnBackToStatisticGen = view.findViewById(R.id.imgBackStatistic);
+        tv_statisticOption = view.findViewById(R.id.statisticOption);
+        tv_AvgTempVal = view.findViewById(R.id.avgTempVal);
+        tv_AvgFanOpTimeVal = view.findViewById(R.id.avgFanOpTimeVal);
+        tv_AvgLightIntenseVal = view.findViewById(R.id.avgLightIntenseVal);
+        tv_AvgLightOpTimeVal = view.findViewById(R.id.avgLightOpTimeVal);
+
+
         // today
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
@@ -91,26 +141,90 @@ public class StatisticsFragment extends Fragment {
                 tvToDate.setText(date);
             }
         };
+
+
+        btnToday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadStatisticSpecLayout();
+                tv_statisticOption.setText("Today");
+                tv_statisticOption.setTextColor(Color.parseColor("#2A954E"));
+            }
+        });
+
+        btnYesterday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadStatisticSpecLayout();
+                tv_statisticOption.setText("Yesterday");
+                tv_statisticOption.setTextColor(Color.parseColor("#CA7240"));
+            }
+        });
+
+        btnThisWeek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadStatisticSpecLayout();
+                tv_statisticOption.setText("This Week");
+                tv_statisticOption.setTextColor(Color.parseColor("#B2B600"));
+            }
+        });
+
+
+        btnThisMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadStatisticSpecLayout();
+                tv_statisticOption.setText("This Month");
+                tv_statisticOption.setTextColor(Color.parseColor("#176BB8"));
+            }
+        });
+
         // button click
         btnGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkDate()) {
-                    loadReport();
-                    tvDateError.setText("");
+                if (checkDate() == "good") {
+                    loadStatisticSpecLayout();
+                    tv_statisticOption.setText(tvFromDate.getText().toString() + " - " + tvToDate.getText().toString());
+                    tv_statisticOption.setTextColor(Color.parseColor("#3CA0FD"));
+//                    loadReport();
                 }
-                else {
+                else if (checkDate() == "invalid") {
                     tvDateError.setText("To-Date must be after From-Date!");
+                }
+                else if (checkDate() == "exceedCurrentDay") {
+                    tvDateError.setText("Date cannot exceed today!");
                 }
             }
         });
+
+
+        btnBackToStatisticGen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutStatisticGen.setVisibility(View.VISIBLE);
+                layoutStatisticSpec.setVisibility(View.GONE);
+            }
+        });
+
+
         return view;
     }
 
-    private void loadReport() {
+    private void loadStatisticSpecLayout() {
+        // change layout
+        layoutStatisticGen.setVisibility(View.GONE);
+        layoutStatisticSpec.setVisibility(View.VISIBLE);
+        tvDateError.setText("");
     }
 
-    private boolean checkDate() {
+    private void loadReport(Date fromDate, Date toDate, String optionValue) {
+
+    }
+
+
+    private String checkDate() {
         String[] fromDate = tvFromDate.getText().toString().split("/");
         String[] toDate = tvToDate.getText().toString().split("/");
 
@@ -121,10 +235,33 @@ public class StatisticsFragment extends Fragment {
         int toMonth = Integer.parseInt(toDate[1]);
         int toDay = Integer.parseInt(toDate[0]);
 
-        if (fromYear > toYear) return false;
-        if (fromYear == toYear && fromMonth > toMonth) return false;
-        if (fromYear == toYear && fromMonth == toMonth && fromDay > toDay) return false;
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        return true;
+//        if (fromYear > year || fromMonth > month + 1 || fromDay > day) return "exceedCurrentDay";
+
+        if (fromYear > year || toYear > year) return "exceedCurrentDay";
+        if ((fromYear == year && fromMonth > month) || (toYear == year && toMonth > month)) return "exceedCurrentDay";
+        if ((fromYear == year && fromMonth == month && fromDay > day) || (toYear == year && toMonth == month && toDay > day)) return "exceedCurrentDay";
+
+        if (fromYear > toYear) return "invalid";
+        if (fromYear == toYear && fromMonth > toMonth) return "invalid";
+        if (fromYear == toYear && fromMonth == toMonth && fromDay > toDay) return "invalid";
+
+        return "good";
     }
+
+    private String[] firstAndLastDaysOfWeek() {
+        String[] result = new String[2];
+        Calendar date = Calendar.getInstance();
+        date.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/YYYY");
+        result[0] = dateFormat.format(date.getTime());
+        date.add(Calendar.DATE, 6);
+        result[1] = dateFormat.format(date.getTime());
+        return result;
+    }
+
 }
